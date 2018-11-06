4dd4d7ca810699addbed578f4ce58f99154eb222
seimiyajun <seimiyajun@gmail.com>
2018-11-06 18:32:50 +0900

    positionの値が配列で渡されているので、Strong Parameterで配列が通るよう変更した

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -69,6 +69,6 @@ class MembersController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def member_params
-      params.require(:member).permit(:name, :position, :introduction)
+      params.require(:member).permit(:name, :introduction, position: [])
     end
 end
